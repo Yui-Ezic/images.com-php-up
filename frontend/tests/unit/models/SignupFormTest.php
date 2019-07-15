008acfdcf2875abcc75d595593f61a1260425680
Yui-Ezic <mishan221199@gmail.com>
2019-07-15 22:22:46 +0300

    Unit tests

--- frontend/tests/unit/models/SignupFormTest.php
+++ frontend/tests/unit/models/SignupFormTest.php
@@ -1,93 +1,84 @@
-<<<<<<< OURS
 <?php 
-namespace frontend\tests\models;
+namespace frontend\tests;
 
+use Yii;
 use frontend\tests\fixtures\UserFixture;
-use frontend\modules\user\models\SignupForm;
 
-class SignupFormTest extends \Codeception\Test\Unit
+class UserTest extends \Codeception\Test\Unit
 {
+
     /**
      * @var \frontend\tests\UnitTester
      */
     protected $tester;
-    
+
+    public function _fixtures()
+    {
+        return ['users' => UserFixture::className()];
+    }
+
     public function _before()
     {
-        $this->tester->haveFixtures([
-            'user' => [
-                'class' => UserFixture::className(),
-            ]
+        Yii::$app->setComponents([
+            'redis' => [
+                'class' => 'yii\redis\Connection',
+                'hostname' => 'localhost',
+                'port' => 6379,
+                'database' => 1,
+            ],
         ]);
+    }
+
+    
+    public function testGetNicknameOnNicknameEmpty()
+    {
+        $user = $this->tester->grabFixture('users', 'user1');
+        expect($user->getNickname())->equals(1);        
     }
     
-    public function testTrimUsername()
+    public function testGetNicknameOnNicknameNotEmpty()
     {
-        $model = new SignupForm([
-            'username' => ' some_username ',
-            'email' => 'some_email@example.com',
-            'password' => '12345',
-        ]);
-
-        $model->signup();
-
-        expect($model->username)
-                ->equals('some_username');
+        $user = $this->tester->grabFixture('users', 'user2');
+        expect($user->getNickname())->equals('catelyn');  
     }
     
-    public function testUsernameRequired()
+    public function testGetPostCount()
     {
-        $model = new SignupForm([
-            'username' => '',
-            'email' => 'some_email@example.com',
-            'password' => 'some_password',
-        ]);
-
-        $model->signup();
-
-        expect($model->getFirstError('username'))
-                ->equals('Username cannot be blank.');
+        $user = $this->tester->grabFixture('users', 'user1');
+        expect($user->getPostCount())->equals(3);  
     }
     
-    public function testUsernameTooShort()
+    public function testFollowUser()
     {
-        $model = new SignupForm([
-            'username' => 's',
-            'email' => 'some_email@example.com',
-            'password' => 'some_password',
-        ]);
-
-        $model->signup();
-
-        expect($model->getFirstError('username'))
-                ->equals('Username should contain at least 2 characters.');
+        $user1 = $this->tester->grabFixture('users', 'user1');
+        $user3 = $this->tester->grabFixture('users', 'user3');
+        
+        $user3->followUser($user1);
+        
+        $this->tester->seeRedisKeyContains('user:1:followers', 3);
+        $this->tester->seeRedisKeyContains('user:3:subscriptions', 1);
+                
+        $this->tester->sendCommandToRedis('del', 'user:1:followers');
+        $this->tester->sendCommandToRedis('del', 'user:3:subscriptions');
     }
     
-    public function testPasswordRequired()
+    public function testUnfollowUser()
     {
-        $model = new SignupForm([
-            'username' => 'some_username',
-            'email' => 'some_email@example.com',
-            'password' => '',
-        ]);
-
-        $model->signup();
-
-        expect($model->getFirstError('password'))
-                ->equals('Password cannot be blank.');
-    }    
-
-    public function testEmailUnique()
-    {
-        $model = new SignupForm([
-            'username' => 'some_username',
-            'email' => '1@got.com',
-            'password' => 'some_password',
-        ]);
-
-        $model->signup();
-
-        expect($model->getFirstError('email'))
-                ->equals('This email address has already been taken.');
+        $user1 = $this->tester->grabFixture('users', 'user1');
+        $user3 = $this->tester->grabFixture('users', 'user3');
+        
+        $user3->followUser($user1);
+        
+        $this->tester->seeRedisKeyContains('user:1:followers', 3);
+        $this->tester->seeRedisKeyContains('user:3:subscriptions', 1);
+        
+        $user3->unfollowUser($user1);
+        
+        $this->tester->dontSeeInRedis('user:1:followers', 3);
+        $this->tester->dontSeeInRedis('user:3:subscriptions', 1);
+                
+        $this->tester->sendCommandToRedis('del', 'user:1:followers');
+        $this->tester->sendCommandToRedis('del', 'user:3:subscriptions');
     }
+
 }
